8919daab860fb8264c6e994191c4f6b40b0ea447
chanyDev <chanyd.dev@gmail.com>
2022-03-18 17:05:14 +0900

    feat : 유저 프로필 이미지 처리 구현

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
 const { login, regist } = require('./userController');
 
 const router = express.Router();
 
 router.post('/login', login);
-router.post('/regist', regist);
+router.post('/regist', upload.single('profileImg'), regist);
 
 module.exports = router;
