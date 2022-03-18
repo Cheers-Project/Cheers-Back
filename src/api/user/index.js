a98f6b31d4e0437b0591fa3c4194bb127d2ab888
kim jung jin <81244738+jungks9351@users.noreply.github.com>
2022-03-18 17:09:53 +0900

    feat : 소셜로그인 라우팅 처리

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -4,6 +4,7 @@ const { login, regist } = require('./userController');
 const router = express.Router();
 
 router.post('/login', login);
+router.post('/login/:social', socialLogin);
 router.post('/regist', regist);
 
 module.exports = router;
