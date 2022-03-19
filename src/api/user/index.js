0a1f3481304559586671edca2c57eb3f1c7912fa
kim jung jin <81244738+jungks9351@users.noreply.github.com>
2022-03-19 17:34:21 +0900

    feat : 카카오 로그인 닉네임 설정하여 JWT 발급

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -5,9 +5,8 @@ const { login, socialLogin, regist } = require('./userController');
 const router = express.Router();
 const upload = multer({ dest: 'uploads/' });
 
-
+router.post('/login', login);
 router.post('/login/:social', socialLogin);
-router.post('/regist', regist);
 router.post('/regist', upload.single('profileImg'), regist);
 
 module.exports = router;
--- src/models/user.js
+++ src/models/user.js
@@ -12,13 +12,13 @@ const userSchema = new Schema({
   },
   userPw: {
     type: String,
-    required: true,
   },
   nickname: {
     type: String,
     required: true,
   },
   profileImg: Object,
+  isSocial: { default: false, type: Boolean },
 });
 
 // 비밀번호 암호화 메서드
@@ -56,6 +56,7 @@ userSchema.methods.generateToken = function () {
 };
 
 // document(문서)가 필요하지 않거나 존재하지 않는 경우 정적 메서드 사용
+// 아이디 확인
 userSchema.statics.checkUser = function (userId) {
   return this.findOne({ userId });
 };
