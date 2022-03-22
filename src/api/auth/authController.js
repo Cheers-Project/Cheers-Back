5479e7c82cde9b112d9a940da31cc39de484800b
kim jung jin <81244738+jungks9351@users.noreply.github.com>
2022-03-23 00:04:57 +0900

    feat : 카카오 토큰 받기

--- src/api/auth/authController.js
+++ src/api/auth/authController.js
@@ -1,3 +1,6 @@
+const { default: axios } = require('axios');
+const qs = require('qs');
+
 // 카카오 인가 코드 받기 요청
 
 exports.kakao = (req, res) => {
@@ -8,6 +11,30 @@ exports.kakao = (req, res) => {
   res.redirect(KAKAO_AUTH_URL);
 };
 
-exports.kakaoCallback = (req, res) => {
-  console.log(req.query);
+exports.kakaoCallback = async (req, res) => {
+  // 카카오 토큰 발급
+  const { code } = req.query;
+  const { KAKAO_API_KEY, KAKAO_REDIRECT_URI, KAKAO_CLIENT_SECRET } =
+    process.env;
+
+  const payload = qs.stringify({
+    grant_type: 'authorization_code',
+    client_id: KAKAO_API_KEY,
+    redirect_uri: KAKAO_REDIRECT_URI,
+    code: code,
+    client_secret: KAKAO_CLIENT_SECRET,
+  });
+
+  try {
+    const {
+      data: { access_token },
+    } = await axios.post(`https://kauth.kakao.com/oauth/token`, payload, {
+      headers: {
+        'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+      },
+    });
+    console.log(access_token);
+  } catch (err) {
+    console.log(err);
+  }
 };
