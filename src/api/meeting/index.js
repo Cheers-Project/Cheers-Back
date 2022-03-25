1a61cf14962940954642ad134cf4360bbf0f79aa
chanyDev <chanyd.dev@gmail.com>
2022-03-25 20:26:29 +0900

    feat : jwt 미들웨어 라우팅 처리

--- src/api/meeting/index.js
+++ src/api/meeting/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const jwtMiddleware = require('../../middleware/jwtMiddleware');
 const { createMeeting, searchNearMeeting } = require('./meetingController');
 
 const router = express.Router();
 
 router.get('/', searchNearMeeting);
-router.post('/', createMeeting);
+router.post('/', jwtMiddleware, createMeeting);
 
 module.exports = router;
