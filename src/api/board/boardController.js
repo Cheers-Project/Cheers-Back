f03b3ba7c651ffcac774a256cb6e82120fc652f4
kim jung jin <81244738+jungks9351@users.noreply.github.com>
2022-04-04 14:56:14 +0900

    feat : 게시물 작성 API 작성 예외처리 필요

---
+++ src/api/board/boardController.js
@@ -0,0 +1,25 @@
+const Board = require('../../models/board');
+
+const jwt = require('jsonwebtoken');
+
+exports.writeBoard = async (req, res) => {
+  const { JWT_SECRET_KEY } = process.env;
+  const token = req.headers.authorization;
+  const { title, contents } = req.body;
+
+  try {
+    const { nickname } = jwt.verify(token, JWT_SECRET_KEY);
+    const board = new Board({
+      title,
+      contents,
+      writer: nickname,
+    });
+
+    console.log(board);
+    await board.save();
+
+    res.status(200).send({ msg: '게시물 작성 완료' });
+  } catch (e) {
+    res.status(500).send({ msg: '게시물 작성 실패' });
+  }
+};
