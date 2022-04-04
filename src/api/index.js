22e290b87b7cb7685761cbb1263fdd2124863ee9
kim jung jin <81244738+jungks9351@users.noreply.github.com>
2022-04-04 14:58:15 +0900

    feat : 게시물 라우팅 생성

--- src/api/index.js
+++ src/api/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const user = require('./user');
 const auth = require('./auth');
 const meeting = require('./meeting');
+const board = require('./board');
 
 const router = express.Router();
 
 router.use('/user', user);
 router.use('/auth', auth);
 router.use('/meeting', meeting);
+router.use('/board', board);
 
 module.exports = router;
