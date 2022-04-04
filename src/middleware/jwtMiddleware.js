b62a8db2b0ae3c53a477b8d1af2f671739884ec6
chanyDev <chanyd.dev@gmail.com>
2022-04-04 19:01:50 +0900

    refactor : 리프레쉬 토큰 확인 시 유저 고유 아이디 확인으로 변경

--- src/middleware/jwtMiddleware.js
+++ src/middleware/jwtMiddleware.js
@@ -16,11 +16,11 @@ const verifyRefreshToken = async (accessToken) => {
   const { JWT_SECRET_KEY } = process.env;
 
   try {
-    const { nickname } = jwt.decode(accessToken, JWT_SECRET_KEY);
-    const user = await User.findByNickname(nickname);
+    const { _id } = jwt.decode(accessToken, JWT_SECRET_KEY);
+
+    const user = await User.findById({ _id });
 
     jwt.verify(user.refreshToken, JWT_SECRET_KEY);
-
     return user;
   } catch (e) {
     return null;
