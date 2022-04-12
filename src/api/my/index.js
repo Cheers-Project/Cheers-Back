481b8bc52db15da3a6e64496cffa7b0366a8f50b
chanyDev <chanyd.dev@gmail.com>
2022-04-12 18:15:35 +0900

    feat : 마이 페이지 라우터 분리 및 내 모임 요청 API 구현

---
+++ src/api/my/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const jwtMiddleware = require('../../middleware/jwtMiddleware');
+
+const {
+  fetchMyMeeting,
+  updateProfileImg,
+  removeProfileImg,
+  updateNickname,
+} = require('./myController');
+
+const s3 = require('../../config/s3');
+const router = express.Router();
+
+const upload = multer({
+  storage: multerS3({
+    s3,
+    bucket: 'lemonalcohol-s3',
+    contentType: multerS3.AUTO_CONTENT_TYPE, // multerS3가 자동으로 파일의 content-type을 지정
+    key: (req, file, cb) => {
+      const fileName = `profile/${Date.now()}_${path.basename(
+        file.originalname,
+      )}`;
+      cb(null, fileName);
+    },
+  }),
+  limits: { fileSize: 20 * 1024 * 1024 },
+});
+
+router.get('/meeting', jwtMiddleware, fetchMyMeeting);
+router.patch('/nickname', jwtMiddleware, updateNickname);
+router.post(
+  '/profile',
+  jwtMiddleware,
+  upload.single('profileImg'),
+  updateProfileImg,
+);
+router.delete('/profile', jwtMiddleware, removeProfileImg);
+
+module.exports = router;
