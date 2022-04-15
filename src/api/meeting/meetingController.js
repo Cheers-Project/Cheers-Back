1b688a9ce2d3a6e06518e14856f27c25da000267
chanyDev <chanyd.dev@gmail.com>
2022-04-15 17:16:15 +0900

    feat : 모임 요청 API 무한 요청 구현

--- src/api/meeting/meetingController.js
+++ src/api/meeting/meetingController.js
@@ -31,27 +31,34 @@ exports.createMeeting = async (req, res) => {
 
 // 모임 얻기
 exports.featchMeeting = async (req, res) => {
-  const { sort } = req.query;
+  const { sort, page } = req.query;
 
   try {
+    const maxPage = Math.ceil((await Meeting.countDocuments({})) / 3);
+    const isLastPage = maxPage - page < 0 ? true : false;
+
     // 최신순
     if (sort === 'recent') {
       const meeting = await Meeting.find({})
         .sort({ createdDate: -1 })
-        .limit(18);
+        .skip((page - 1) * 3)
+        .limit(3);
 
-      return res.status(200).send({ meeting });
+      return res.status(200).send({ meeting, isLastPage });
     }
     // 조회수순
     if (sort === 'view') {
-      const meeting = await Meeting.find({}).sort({ view: -1 }).limit(18);
+      const meeting = await Meeting.find({})
+        .sort({ view: -1 })
+        .skip((page - 1) * 3)
+        .limit(3);
 
-      return res.status(200).send({ meeting });
+      return res.status(200).send({ meeting, isLastPage });
     }
+
     // 근처 모임
     if (sort === 'near') {
       const { lon, lat } = req.query;
-
       if (lon && lat) {
         const meeting = await Meeting.find({
           location: {
@@ -65,11 +72,12 @@ exports.featchMeeting = async (req, res) => {
           },
         });
 
-        return res.status(200).send({ meeting });
+        return res.status(200).send({ meeting, isLastPage: true });
       } else {
-        return res
-          .status(403)
-          .send({ msg: '위치 정보가 활성화 되어 있지 않습니다.' });
+        return res.status(403).send({
+          meeting: null,
+          msg: '위치 서비스 권한을 설정해주세요.',
+        });
       }
     }
 
