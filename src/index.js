ceee70d07754062d3c1678b39499d94d30e573cd
chanyDev <chanyd.dev@gmail.com>
2022-05-03 13:38:41 +0900

    feat : cors 설정 추가

--- src/index.js
+++ src/index.js
@@ -23,7 +23,12 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  }),
+);
 
 app.use('/api', api);
 
